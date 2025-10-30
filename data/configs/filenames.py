e8221cc19c413496f46b30cacb6c398015ad8356
mathali <matej.halinkovic@gmail.com>
2025-10-30 15:46:53 +0100

    M

--- data/configs/filenames.py
+++ data/configs/filenames.py
@@ -1,5 +1,5 @@
 
-REPO_ROOT = r"C:\Users\Lukas\richard\xmem_e2e\XMem"
+REPO_ROOT = "XMem"
 XMEM_CHECKPOINT = "XMem/XMem-s012.pth"
 XMEM_CONFIG = "data/configs/xmem_config.json"
 TRAIN_CONFIG  = "data/configs/train_config.json"
